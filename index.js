3cca1d582489586e651f6e8d8f97c16945319fd2
Elisha Sunday <129615551+Elilaw179@users.noreply.github.com>
2024-12-14 05:23:11 +0100

    Add files via upload
    added files

--- index.js
+++ index.js
@@ -4,11 +4,90 @@ let navColumn = document.getElementById("navColumn");
 
 list.addEventListener("click", () => {
   navColumn.classList.add("active");
-
 });
 
 x.addEventListener("click", () => {
   navColumn.classList.remove("active");
+});
 
+function createCard({ imgSrc, author, title, metaImg, oldPrice, newPrice }) {
+  return `
+        <div class="ft-card">
+          <div class="ft-card-img">
+            <img src="${imgSrc}" alt="img" />
+          </div>
+          <div class="ft-card-txt">
+            <p>by ${author}</p>
+            <h5>${title}</h5>
+          </div>
+          <div class="ft-date">
+            <img src="${metaImg}" alt="Meta image" />
+          </div>
+          <div class="amount">
+            <div>
+              <s>$${oldPrice}</s>
+              <b>${newPrice}</b>
+            </div>
+            <button>View More</button>
+          </div>
+        </div> 
+       
+       `;
+}
 
+const cardsData = [
+  {
+    imgSrc: "Image (1).png",
+    author: "Determined-Poitras",
+    title: "Create an LMS Website with LearnPress",
+    metaImg: "Meta.png",
+    oldPrice: "29.0",
+    newPrice: "Free",
+  },
+  {
+    imgSrc: "Image (2).png",
+    author: "Determined-Poitras",
+    title: "Create an LMS Website with LearnPress",
+    metaImg: "Meta.png",
+    oldPrice: "29.0",
+    newPrice: "Free",
+  },
+  {
+    imgSrc: "Image (3).png",
+    author: "Determined-Poitras",
+    title: "Create an LMS Website with LearnPress",
+    metaImg: "Meta.png",
+    oldPrice: "29.0",
+    newPrice: "Free",
+  },
+  {
+    imgSrc: "Image (4).png",
+    author: "Determined-Poitras",
+    title: "Create an LMS Website with LearnPress",
+    metaImg: "Meta.png",
+    oldPrice: "29.0",
+    newPrice: "Free",
+  },
+  {
+    imgSrc: "Image (5).png",
+    author: "Determined-Poitras",
+    title: "Create an LMS Website with LearnPress",
+    metaImg: "Meta.png",
+    oldPrice: "29.0",
+    newPrice: "Free",
+  },
+  {
+    imgSrc: "Image (6).png",
+    author: "Determined-Poitras",
+    title: "Create an LMS Website with LearnPress",
+    metaImg: "Meta.png",
+    oldPrice: "29.0",
+    newPrice: "Free",
+  },
+];
+
+const cardContainer = document.getElementById("card-container");
+
+cardsData.forEach((cardData) => {
+  cardContainer.innerHTML += createCard(cardData);
 });
